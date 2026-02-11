8ddab1b828f828cf6a8ab3af159e4482ba94f662
Yannick Remy <remyyannick@sfr.fr>
2026-02-11 11:38:45 +0100

    feat(events): ajout du formulaire de création d’événement

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError
 
 app = Flask(__name__)
+app.secret_key = "metz-numeric-school"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 
 db = SQLAlchemy(app)
@@ -41,3 +42,37 @@ def list_events():
     events = Event.query.all()
 
     return render_template("home.html", events=events)
+
+
+@app.get("/evenements/ajouter")
+def create_event_get():
+    return render_template("event-form.html")
+
+
+@app.post("/evenements/ajouter")
+def create_event_post():
+    data = request.form
+
+    try:
+        validated_data = CreateEventRequest.model_validate(data)
+    except ValidationError as e:
+        errors = {}
+
+        for error in e.errors():
+            field = error["loc"][0]
+            message = error["msg"]
+            errors[field] = message
+
+        return render_template("event-form.html", errors=errors, old=data)
+
+    new_event = Event()
+    new_event.title = validated_data.title
+    new_event.type = validated_data.type
+    new_event.date = validated_data.date
+    new_event.location = validated_data.location
+    new_event.description = validated_data.description
+
+    db.session.add(new_event)
+    db.session.commit()
+
+    return redirect(url_for("list_events"))
